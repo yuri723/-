337ed8755572952e36dad2e9de575e11c4c4f951
TaigaK <backnum5509@gmail.com>
2017-10-09 22:21:14 +0700

    add: 前処理

---
+++ platforms/ios/www/js/policy3.js
@@ -0,0 +1,23 @@
+
+
+$(document).ready(function(){
+  $('#send').on('click',function(){
+    var answer = $('input[name=answer]:checked').val();
+    console.log(answer);
+    $.ajax("https://support-spouses-communication.herokuapp.com/v1/question_answers",{
+        type: 'POST',
+        headers: {
+          'Authorization': window.localStorage.getItem("access_token_local"),
+          'UserType': window.localStorage.getItem("seibetu_local"),
+        },
+        data: {"question_id": 3, "answer": answer},
+        dataType: 'json',
+        timeout: 10000,
+    }).done(function(data) {
+      console.log(data);
+      window.location.href = 'policy4.html';
+    }).fail(function(jqXHR, statusText, errorThrown) {
+      alert("エラー");
+    });
+  });
+});
---
+++ www/js/policy4.js
@@ -0,0 +1,23 @@
+
+
+$(document).ready(function(){
+  $('#send').on('click',function(){
+    var answer = $('input[name=answer]:checked').val();
+    console.log(answer);
+    $.ajax("https://support-spouses-communication.herokuapp.com/v1/question_answers",{
+        type: 'POST',
+        headers: {
+          'Authorization': window.localStorage.getItem("access_token_local"),
+          'UserType': window.localStorage.getItem("seibetu_local"),
+        },
+        data: {"question_id": 4, "answer": answer},
+        dataType: 'json',
+        timeout: 10000,
+    }).done(function(data) {
+      console.log(data);
+      window.location.href = 'sharing.html';
+    }).fail(function(jqXHR, statusText, errorThrown) {
+      alert("エラー");
+    });
+  });
+});
