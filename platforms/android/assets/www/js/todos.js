8d445213010b30cb74b513ceef49e9503bed83d9
yuri723 <yurisimo723@gmail.com>
2017-10-20 02:08:25 +0900

    todo見た目

--- platforms/android/assets/www/js/todos.js
+++ platforms/android/assets/www/js/todos.js
@@ -8,8 +8,14 @@ $(document).ready(function(){
       timeout: 10000,
   }).done(function(data) {
     data.forEach(function(val,index,ar){
+      var time_unix = val.deadline ;
+      var time_year = time_unix.substr(0,4);
+      var time_month = time_unix.substr(5,2);
+      var time_date = time_unix.substr(8,2);
+      var time_ymd = time_year+"/"+time_month + "/"+time_date ;
+
       $("#must_todos").append("<div>")
-      $("#must_todos").append("締め切り:"+val.deadline+"<br>内容:"+val.statement+"<br>作成者:"+val.user_type)
+      $("#must_todos").append("【締め切り】"+time_ymd+"<br>【内容】"+val.statement+"<br>【作成者】"+val.user_type)
       $("#must_todos").append("</div>")
       $("#must_todos").append("<hr>")
      });
@@ -28,8 +34,14 @@ $(document).ready(function(){
       timeout: 10000,
   }).done(function(data) {
     data.forEach(function(val,index,ar){
+      var time_unix = val.deadline ;
+      var time_year = time_unix.substr(0,4);
+      var time_month = time_unix.substr(5,2);
+      var time_date = time_unix.substr(8,2);
+      var time_ymd = time_year+"/"+time_month + "/"+time_date ;
+
       $("#want_todos").append("<div>")
-      $("#want_todos").append("締め切り:"+val.deadline+"<br>内容:"+val.statement+"<br>作成者:"+val.user_type)
+      $("#want_todos").append("【締め切り】"+time_ymd+"<br>【内容】"+val.statement+"<br>【作成者】"+val.user_type)
       $("#want_todos").append("</div>")
       $("#want_todos").append("<hr>")
      });
